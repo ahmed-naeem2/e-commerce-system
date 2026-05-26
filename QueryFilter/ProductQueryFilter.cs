55f785edb2d35ad28747267e90a03ffed72997ca
Ahmed naeem <hnym57@gmail.com>
2026-05-26 21:19:03 +0300

    add QueryFilter For Product

---
+++ QueryFilter/ProductQueryFilter.cs
@@ -0,0 +1,18 @@
+﻿namespace e_commerce_system.QueryFilter
+{
+	public class ProductQueryFilter
+	{
+
+		public int PageNumber { get; set; } = 1;
+		public int PageSize { get; set; } = 10;	
+		public string? Search {  get; set; }
+
+		public string? FilterByCatogrie { get; set; }
+
+		public String ? SortBy { get; set; }
+
+
+
+
+	}
+}
