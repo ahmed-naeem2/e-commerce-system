b342df3f4b543eacff1a6b0d5e71c4ac69f629ad
Ahmed naeem <hnym57@gmail.com>
2026-05-26 21:29:59 +0300

    Add Applay Pagination In QueryExtion Class

---
+++ ExtensionMethods/QueryExtension.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace e_commerce_system.ExtensionMethods
+{
+	public static class QueryExtension
+	{
+
+		public static IQueryable<T> ApplayPagination<T>(this IQueryable<T> query, int pageNumber, int pageSize) 
+		{
+		return	query.Skip((pageNumber-1)*pageSize)
+				.Take(pageSize);
+				
+
+
+		}
+
+	}
+}
