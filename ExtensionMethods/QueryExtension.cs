89c3338df5e9dc6a24229bf2a3d34cec8376b894
Ahmed naeem <hnym57@gmail.com>
2026-05-26 22:36:19 +0300

    Add Applay Sort In Query Extenision

--- ExtensionMethods/QueryExtension.cs
+++ ExtensionMethods/QueryExtension.cs
@@ -1,11 +1,14 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Linq.Dynamic.Core;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace e_commerce_system.ExtensionMethods
 {
 	public static class QueryExtension
 	{
 
-		public static IQueryable<T> ApplayPagination<T>(this IQueryable<T> query, int pageNumber, int pageSize) 
+		public static IQueryable<T> ApplayPagination<T>(this IQueryable<T> query, int pageNumber, int pageSize) where T : class
 		{
 		return	query.Skip((pageNumber-1)*pageSize)
 				.Take(pageSize);
@@ -14,5 +17,36 @@ namespace e_commerce_system.ExtensionMethods
 
 		}
 
+		public static IQueryable<T> ApplaySort <T>(this IQueryable<T> query,string? SortBy)where T : class
+		{
+			if(string .IsNullOrEmpty(SortBy))
+
+					return query;
+
+			var allwedPropeties = typeof(T)
+				.GetProperties(BindingFlags.Public | BindingFlags.Instance)
+				.Select(p => p.Name)
+				.ToHashSet(StringComparer.OrdinalIgnoreCase);
+			var sortExpressions = new List<string>();
+			foreach(var part in SortBy.Split(",",StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries))
+			{
+
+				var tokens = part.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+				if( tokens.Length==0 ||!allwedPropeties.Contains(tokens[0]))
+					continue;
+
+				var direction = tokens.Length > 1 && tokens[1].Equals("desc", StringComparison.OrdinalIgnoreCase) ?
+					"descending"
+					: "ascending";
+
+				sortExpressions.Add($"{tokens[0]} {direction}");
+			}
+			return sortExpressions.Count > 0
+		? query.OrderBy(string.Join(", ", sortExpressions))
+		: query;
+		}
+
+
+		
 	}
 }
